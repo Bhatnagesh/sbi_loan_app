65507422aa84e3bdff2f8d38457137ad57f172f8
Nagesh Bhat <153530869+Bhatnagesh@users.noreply.github.com>
2025-04-22 12:30:10 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -3,5 +3,7 @@ class Demo
   public static void main(String[] args)
     {
               System.out.println("Apr 22 2024");
+              System.out.println("Apr 23 2024");
+                
     }
   }
